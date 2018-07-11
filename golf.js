892f9af01ebba1f9669b2aa43b68995e802172c5
Ihor Rudych <39833363+IhorRudych@users.noreply.github.com>
2018-07-11 14:05:16 -0600

    some updates

--- golf.js
+++ golf.js
@@ -1,8 +1,13 @@
 let players = [{name:"Gordon", par:89, course:"ParkCity"},
 {name:"London", par:82, course:"ParkCity"},
+{name:"Ramona", par:97, course:"HeberCity"},
 {name:"Hordon", par:99, course:"ParkCity"},
+{name:"Lincoln", par:87, course:"OakCity"},
+{name:"Steph", par:89, course:"ParkCity"},
 {name:"Corban", par:109, course:"ParkCity"}];
-//console.log(players);                                                                                                                         
+
+//console.log(players); 
+
 let sortGolfers = function(data, callback) {
 	for (let i = 0; i < data.length; i++) {
 		if (data[i].course.toLowerCase() === "ParkCity"){
@@ -16,7 +21,7 @@ let sortGolfers = function(data, callback) {
 sortGolfers(players, function(obj) {
 	console.log(players);
 	if (obj.par > 90){
-		console.log(obj.name + "bad golfer.");
+		console.log(obj.name + "is good golfer.");
 	}
 });
 
